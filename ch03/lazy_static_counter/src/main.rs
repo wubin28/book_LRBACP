25b4876cb0c7b8c920d4adc11c0a65181229f117
wuzhenben <wzb@wuzhenben.com>
2024-09-24 22:12:44 +0800

    feat:ch03:lazy_static_counter: Add lazy_static reference to static counter
    - Introduced the 'lazy_static' crate to create a static reference to the 'COUNTER' variable.
    - Defined a 'REFERENCE' using 'lazy_static' to hold a reference to the 'COUNTER'.
    - Updated the 'main' function to print the value of 'REFERENCE'.
    
    This change ensures that the reference to the static counter is properly initialized at runtime.

--- ch03/lazy_static_counter/src/main.rs
+++ ch03/lazy_static_counter/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use lazy_static::lazy_static;
+
+static COUNTER: i32 = 0;
+
+lazy_static! {
+    static ref REFERENCE: &'static i32 = &COUNTER;
 }
+
+fn main() {
+    println!("Reference value: {}", *REFERENCE);
+}
+// 输出：
+// Reference value: 0
