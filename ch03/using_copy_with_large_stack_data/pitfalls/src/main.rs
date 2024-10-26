837c645cc65d34b7aaf8bee18f921de341b54f6d
wuzhenben <wzb@wuzhenben.com>
2024-10-26 19:46:17 +0800

    pitfalls:ch03:using_copy_with_large_stack_data:pitfalls

--- ch03/using_copy_with_large_stack_data/pitfalls/src/main.rs
+++ ch03/using_copy_with_large_stack_data/pitfalls/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Clone, Copy, Debug)]
+struct MyStruct {
+    data: [u8; 1000000],
 }
+
+fn main() {
+    let s1 = MyStruct { data: [0; 1000000] };
+    let s2 = s1;
+    println!("s1.data[0]: {} \ns2.data[0]: {}", s1.data[0], s2.data[0]);
+}
+// 运行输出：
+// s1.data[0]: 0
+// s2.data[0]: 0
