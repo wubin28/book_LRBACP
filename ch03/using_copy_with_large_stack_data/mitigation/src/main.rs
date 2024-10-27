38296e912a6fe137eed98865d4a85f3affd751bc
wuzhenben <wzb@wuzhenben.com>
2024-10-27 20:17:25 +0800

    mitigation:ch03:using_copy_with_large_stack_data:mitigation: use Vec to store data on the heap to avoid stack overflow

--- ch03/using_copy_with_large_stack_data/mitigation/src/main.rs
+++ ch03/using_copy_with_large_stack_data/mitigation/src/main.rs
@@ -1,19 +1,15 @@
-// MyStruct结构体里有大型数组，不适合实现Copy trait
-// Copy trait仅用于简单和轻量的按位复制，而不适用于大型数据结构
-// 正确写法应该是：#[derive(Clone)]
 #[derive(Clone)]
 struct MyStruct {
-    // 大型数组
-    data: [u8; 1000000],
+    data: Vec<u8>, // 数据存储在堆上，栈上只存储Vec的头部信息
 }
 
 fn main() {
-    let s1 = MyStruct { data: [0; 1000000] };
-    // 正确写法应该是：let s2 = s1.clone();
-    // 调用clone()方法的好处是显式地提醒程序员这里要进行昂贵的克隆操作
-    let s2 = s1.clone();
+    let s1 = MyStruct {
+        data: vec![0; 1000000], // 数据分配在堆上
+    };
+    let s2 = s1.clone(); // 只在堆上创建新的数据副本，栈上开销很小
     println!("s1.data[0]: {} \ns2.data[0]: {}", s1.data[0], s2.data[0]);
 }
 // 运行输出：
-// s1.data[0]: 0
+// s1.data[0]: 0 
 // s2.data[0]: 0
