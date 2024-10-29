d1a02731a9014b0f48d9a0d08e7792837785fec5
wuzhenben <wzb@wuzhenben.com>
2024-10-29 12:07:11 +0800

    mitigation:ch01:avoid_dangling_ptrs_with_refs: add explanatory comments

--- ch01/avoid_dangling_ptrs_with_refs/src/main.rs
+++ ch01/avoid_dangling_ptrs_with_refs/src/main.rs
@@ -1,39 +1,26 @@
 fn main() {
     println!("Rust 避免悬空指针示例开始运行...");
 
+    // 声明名为reference的变量，但暂不初始化
     let reference;
 
     {
-        let smart_ptr = Box::new(42);
+        // 开始一个新作用域
+        // 绑定类型为Box<i32>的智能指针smart_ptr，存储整数值55，Box在堆上分配内存
+        let smart_ptr = Box::new(55);
+        // 变量reference的类型在这里进行初始化时才会被推断出来
+        // 首先*解引用操作符解引用smart_ptr，获取存储在Box中的实际整数值55
+        // 然后&引用创建操作符创建55这个整数值的引用
+        // 最后将这个引用初始化给变量reference
         reference = &*smart_ptr;
 
         println!("智能指针管理的值: {}", smart_ptr);
 
         println!("引用指向的值: {}", reference);
-    } // smart_ptr 在此作用域结束后被销毁
+    } // smart_ptr在此作用域结束后被销毁
 
-    // 尝试使用 reference 会导致编译错误
+    // 尝试使用悬垂引用reference会导致编译错误
     println!("引用指向的值: {}", reference);
 }
-// 'cargo run' Output (注释掉第16行):
-// Rust 避免悬空指针示例开始运行...
-// 智能指针管理的值: 42
-// 引用指向的值: 42
-//
-// 'cargo build' Output (去掉第16行注释):
+// 'cargo build' 编译输出:
 // error[E0597]: `*smart_ptr` does not live long enough
-//   --> src/main.rs:8:21
-//    |
-// 7  |         let smart_ptr = Box::new(42);
-//    |             --------- binding `smart_ptr` declared here
-// 8  |         reference = &*smart_ptr;
-//    |                     ^^^^^^^^^^^ borrowed value does not live long enough
-// ...
-// 13 |     } // smart_ptr 在此作用域结束后被销毁
-//    |     - `*smart_ptr` dropped here while still borrowed
-// ...
-// 16 |     println!("引用指向的值: {}", reference);
-//    |                                  --------- borrow later used here
-//
-// For more information about this error, try `rustc --explain E0597`.
-// error: could not compile `dangling_danger_rust` (bin "dangling_danger_rust") due to 1 previous error
