7696727b759f2fefa8797437084a264d36a7e996
wuzhenben <wzb@wuzhenben.com>
2024-10-31 23:06:38 +0800

    pitfalls:ch02:shadowed_mutability_trap

--- ch02/shadowed_mutability_trap/src/main.rs
+++ ch02/shadowed_mutability_trap/src/main.rs
@@ -1,20 +1,9 @@
 fn main() {
     let mut count = 0;
-    println!("初始 count: {}", count);
 
-    // 遮蔽为不可变
     let count = count + 1;
-    println!("遮蔽后的 count: {}", count);
 
-    // 试图修改 count，会导致编译错误
-    count += 1; // 错误：count 是不可变的
+    count += 1;
 }
 // 编译错误：
 // error[E0384]: cannot assign twice to immutable variable `count`
-// --> src/main.rs:10:5
-// |
-// 6  |     let count = count + 1;
-// |         ----- first assignment to `count`
-// ...
-// 10 |     count += 1; // 错误：count 是不可变的
-// |     ^^^^^^^^^^ cannot assign twice to immutable variable
