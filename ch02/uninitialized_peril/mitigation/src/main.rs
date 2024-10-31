29b1965f758901a966a992458f2fe6fa2299341a
wuzhenben <wzb@wuzhenben.com>
2024-10-31 20:21:56 +0800

    mitigation:ch02:uninitialized_peril:mitigation

--- ch02/uninitialized_peril/mitigation/src/main.rs
+++ ch02/uninitialized_peril/mitigation/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    // 声明了不可变变量x，但暂未初始化
+    let x: i32;
+    // true是一个布尔字面值，表示条件永远成立
+    if true {
+        // 将不可变变量x初始化为5，
+        // 因为条件为true，所以这行代码一定会执行
+        x = 5;
+    } else {
+        unreachable!();
+    }
+    println!("x的值是: {}", x);
 }
+// 运行输出：
+// x的值是: 5
