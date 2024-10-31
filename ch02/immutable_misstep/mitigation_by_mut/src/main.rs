704dca142aa0f8494eabfee258963e4ddbc76c88
wuzhenben <wzb@wuzhenben.com>
2024-10-31 13:25:51 +0800

    mitigation:ch02:immutable_misstep:mitigation_by_mut

--- ch02/immutable_misstep/mitigation_by_mut/src/main.rs
+++ ch02/immutable_misstep/mitigation_by_mut/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    // 声明了可变变量sum并初始化为0，
+    // 也就是将0绑定到可变变量sum上
+    let mut sum = 0;
+    for i in 1..=3 {
+        // sum自增i
+        sum += i;
+    }
+    println!("求和结果: {}\n", sum);
 }
+// 运行输出:
+// 求和结果: 6
