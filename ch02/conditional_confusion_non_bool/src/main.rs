87287ac8dfcbd2ed6e48ebf7e07119db020c0370
wuzhenben <wzb@wuzhenben.com>
2024-11-01 17:10:03 +0800

    pitfalls:ch02:conditional_confusion_non_bool

--- ch02/conditional_confusion_non_bool/src/main.rs
+++ ch02/conditional_confusion_non_bool/src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
     let number = 5;
 
-    // 错误：期望bool类型，得到i32类型
     if number {
         println!("数字是真的？");
     }
@@ -9,7 +8,3 @@ fn main() {
 }
 // 编译错误：
 // error[E0308]: mismatched types
-//  --> src/main.rs:5:8
-//   |
-// 5 |     if number {
-//   |        ^^^^^^ expected `bool`, found integer
