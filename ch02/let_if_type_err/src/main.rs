c42f7ffa423053c10d3b5046a228f55951a4035f
wuzhenben <wzb@wuzhenben.com>
2024-11-01 11:25:30 +0800

    pitfalls:ch02:let_if_type_error

--- ch02/let_if_type_err/src/main.rs
+++ ch02/let_if_type_err/src/main.rs
@@ -1,25 +1,11 @@
 fn main() {
     let condition = true;
 
-    // 错误示例
-    let number = if condition {
-        5
-    } else {
-        "six" // 这里使用了字符串,与if分支的整数类型不匹配
-    };
+    // 使用if表达式为变量number绑定值，
+    // 如果condition为true，则number绑定5，否则绑定"six"
+    let number = if condition { 5 } else { "six" };
 
     println!("The value of number is: {}", number);
 }
 // 编译错误：
 // error[E0308]: `if` and `else` have incompatible types
-//  --> main.rs:8:9
-//   |
-// 5 |       let number = if condition {
-//   |  __________________-
-// 6 | |         5
-//   | |         - expected because of this
-// 7 | |     } else {
-// 8 | |         "six" // 这里使用了字符串,与if分支的整数类型不匹配
-//   | |         ^^^^^ expected integer, found `&str`
-// 9 | |     };
-//   | |_____- `if` and `else` have incompatible types
