37711dc143df8845e7a5ffda62230ebc20684e31
wuzhenben <wzb@wuzhenben.com>
2024-11-01 14:51:11 +0800

    pitfalls:ch02:let_if_unhandled_case

--- ch02/let_if_unhandled_case/src/main.rs
+++ ch02/let_if_unhandled_case/src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
     let number = 7;
 
-    // 错误示例
     let result = if number < 5 {
         "less than five"
     } else if number > 10 {
@@ -12,14 +11,3 @@ fn main() {
 }
 // 编译错误：
 // error[E0317]: `if` may be missing an `else` clause
-//  --> main.rs:7:12
-//   |
-// 7 |       } else if number > 10 {
-//   |  ____________^
-// 8 | |         "greater than ten"
-//   | |         ------------------ found here
-// 9 | |     };
-//   | |_____^ expected `()`, found `&str`
-//   |
-//   = note: `if` expressions without `else` evaluate to `()`
-//   = help: consider adding an `else` block that evaluates to the expected type
