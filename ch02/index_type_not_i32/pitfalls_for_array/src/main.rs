7f71eeb01b050f1d2eea166973a4ae0e71463424
wuzhenben <wzb@wuzhenben.com>
2024-11-02 20:41:18 +0800

    pitfalls:ch02:index_type_not_i32:pitfalls_for_array

--- ch02/index_type_not_i32/pitfalls_for_array/src/main.rs
+++ ch02/index_type_not_i32/pitfalls_for_array/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let array = [10, 20, 30, 40, 50];
+    let index: i32 = 2;
+    println!("array[{}] = {}", index, array[index]);
 }
+// 编译错误：
+// error[E0277]: the type `[{integer}]` cannot be indexed by `i32`
