cc95c80f3d8cafd6d646f8e67ca27cf98dcf07e8
wuzhenben <wzb@wuzhenben.com>
2024-11-04 23:04:18 +0800

    pitfalls:ch02:access_char_in_str_using_index:pitfalls

--- ch02/access_char_in_str_using_index/pitfalls/src/main.rs
+++ ch02/access_char_in_str_using_index/pitfalls/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let string = String::from("你好");
+    let ch = string[0];
+    println!("第一个字符是: {}", ch);
 }
+// 编译错误：
+// error[E0277]: the type `String` cannot be indexed by `{integer}`
