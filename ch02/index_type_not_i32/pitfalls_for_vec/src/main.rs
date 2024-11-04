289b0e7b9f0d80f328b7cea902c280713ba73cf4
wuzhenben <wzb@wuzhenben.com>
2024-11-04 15:29:01 +0800

    pitfalls:ch02:index_type_not_i32:pitfalls_for_vec

--- ch02/index_type_not_i32/pitfalls_for_vec/src/main.rs
+++ ch02/index_type_not_i32/pitfalls_for_vec/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let vec = vec![10, 20, 30, 40, 50];
+    let index: i32 = 3;
+    println!("vec[{}] = {}", index, vec[index]);
 }
+// 编译错误：
+// error[E0277]: the type `[{integer}]` cannot be indexed by `i32`
