c62e440f307e096c843002707275531231626fd1
wuzhenben <wzb@wuzhenben.com>
2024-11-04 15:15:10 +0800

    pitfalls:ch02:index_type_not_i32:pitfalls_for_slice

--- ch02/index_type_not_i32/pitfalls_for_slice/src/main.rs
+++ ch02/index_type_not_i32/pitfalls_for_slice/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let array = [10, 20, 30, 40, 50];
+    let slice = &array[1..4]; //取得数组的一个切片
+    let index: i32 = 1;
+    println!("slice[{}] = {}", index, slice[index]);
 }
+// 编译错误：
+// error[E0277]: the type `[{integer}]` cannot be indexed by `i32`
