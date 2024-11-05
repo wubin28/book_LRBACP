03ca1bd8eea6cc7564f5ba06c9a60dbf25ba17be
wuzhenben <wzb@wuzhenben.com>
2024-11-05 21:15:01 +0800

    how-to:ch02:char_not_1_byte_long

--- ch02/char_not_1_byte_long/src/main.rs
+++ ch02/char_not_1_byte_long/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let chinese_text = String::from("第2章");
+
+    for ch in chinese_text.chars() {
+        println!("{}所占字节数：{}", ch, std::mem::size_of_val(&ch));
+    }
+
+    println!("字符数量: {}", chinese_text.chars().count());
 }
+// 运行输出：
+// 第所占字节数：4
+// 2所占字节数：4
+// 章所占字节数：4
+// 字符数量: 3
