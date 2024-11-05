a7816499e4c7cfe6d19fb3d2d28160a848172491
wuzhenben <wzb@wuzhenben.com>
2024-11-05 10:34:19 +0800

    how-to:ch02:bind_value_of_non_default_inferred_type

--- ch02/bind_value_of_non_default_inferred_type/src/main.rs
+++ ch02/bind_value_of_non_default_inferred_type/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    // 在变量声明时直接标注类型
+    let integer: u8 = 55;
+    println!("55的类型是: {}", std::any::type_name_of_val(&integer));
+
+    // 在数值字面量后添加类型后缀
+    let float = 3.14f32;
+    println!("3.14的类型是: {}", std::any::type_name_of_val(&float));
 }
+// 运行输出：
+// 55的类型是: u8
+// 3.14的类型是: f32
