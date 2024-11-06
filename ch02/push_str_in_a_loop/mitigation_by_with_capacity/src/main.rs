3dfccaaad73e9bd72bba254d30d3efe40e00cbfa
wuzhenben <wzb@wuzhenben.com>
2024-11-06 09:56:27 +0800

    mitigation:ch02:push_str_in_a_loop:mitigation_by_with_capacity

--- ch02/push_str_in_a_loop/mitigation_by_with_capacity/src/main.rs
+++ ch02/push_str_in_a_loop/mitigation_by_with_capacity/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let start = std::time::Instant::now();
+    let mut s1 = String::with_capacity(10000);
+    for _ in 0..10000 {
+        s1.push_str("a");
+    }
+    println!("耗时: {:?}", start.elapsed());
 }
+// 运行输出：
+// 耗时: 634.042µs
