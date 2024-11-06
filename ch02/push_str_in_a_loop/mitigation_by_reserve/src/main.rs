80b91dcd40513f507f963b830ced8e3d0c0b135c
wuzhenben <wzb@wuzhenben.com>
2024-11-06 10:01:25 +0800

    mitigation:ch02:push_str_in_a_loop:mitigation_by_reserve

--- ch02/push_str_in_a_loop/mitigation_by_reserve/src/main.rs
+++ ch02/push_str_in_a_loop/mitigation_by_reserve/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let start = std::time::Instant::now();
+    let mut s1 = String::new();
+    // 确保字符串至少有10000字节的可用容量，
+    // 如果当前容量小于10000，它会重新分配内存，
+    // 但不会改变字符串的内容和长度，只增加容量
+    s1.reserve(10000);
+    for _ in 0..10000 {
+        s1.push_str("a");
+    }
+    println!("耗时: {:?}", start.elapsed());
 }
+// 运行输出：
+// 耗时: 636.667µs
