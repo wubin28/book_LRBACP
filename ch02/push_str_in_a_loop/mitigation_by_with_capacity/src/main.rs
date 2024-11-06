523e568ca45afb52eb7d909be5b56784cf8d7f8a
wuzhenben <wzb@wuzhenben.com>
2024-11-06 10:03:41 +0800

    docs:ch02:push_str_in_a_loop:mitigation_by_with_capacity

--- ch02/push_str_in_a_loop/mitigation_by_with_capacity/src/main.rs
+++ ch02/push_str_in_a_loop/mitigation_by_with_capacity/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     let start = std::time::Instant::now();
+    // with_capacity()是String类型的构造函数，
+    // 用于创建一个新字符串并预分配指定大小的内部缓冲区，
+    // 参数10000表示预分配10000字节的容量
     let mut s1 = String::with_capacity(10000);
     for _ in 0..10000 {
         s1.push_str("a");
